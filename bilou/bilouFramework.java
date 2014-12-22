7283428917e000463f21a17fd68d6c7dcfa1c841
cedric thonon <thonon_cedric@hotmail.com>
2014-12-22 20:34:05 +0100

    creatin du framework

---
+++ bilou/bilouFramework.java
@@ -0,0 +1,31 @@
+package bilou;
+
+import org.jsfml.window.event.Event;
+
+public class bilouFramework 
+{
+	public void Update()
+	{
+		
+	}
+	
+	public void Draw()
+	{
+		
+	}
+	
+	public void CatchEvent(Event event)
+	{
+		
+	}
+	
+	public void LoadContent()
+	{
+		
+	}
+	
+	public void ReleaseContent()
+	{
+		
+	}
+}
