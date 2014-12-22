03b26fe405f9434ecf8e74fac29d8b2cab723212
cedric thonon <thonon_cedric@hotmail.com>
2014-12-22 20:26:43 +0100

    debut de devellopement

---
+++ bilou/mainProgram.java
@@ -0,0 +1,38 @@
+package bilou;
+
+import org.jsfml.graphics.Color;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
+public class mainProgram {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//Create the window
+		RenderWindow window = new RenderWindow();
+		window.create(new VideoMode(800, 600), "Hello JSFML!");
+
+		//Limit the framerate
+		window.setFramerateLimit(30);
+
+		//Main loop
+		while(window.isOpen()) {
+		    //Fill the window with red
+		    window.clear(Color.RED);
+
+		    //Display what was drawn (... the red color!)
+		    window.display();
+
+		    //Handle events
+		    for(Event event : window.pollEvents()) {
+		        if(event.type == Event.Type.CLOSED) {
+		            //The user pressed the close button
+		            window.close();
+		        }
+		    }
+		}
+	}
+
+}
