e59ea6226b1f47438485354cfa87ab63bf34925c
THOCED <thonon_cedric@hotmail.com>
2014-12-24 13:07:27 +0100

    Creation du QuadTreeNode (node de l'arbre QuadTree)

---
+++ CoreQuadTree/QuadTreeNode.java
@@ -0,0 +1,86 @@
+package CoreQuadTree;
+
+import java.util.ArrayList;
+
+import org.jsfml.graphics.FloatRect;
+
+import bilou.IGameBase;
+
+public class QuadTreeNode 
+{
+	// bounds du noeud
+	private FloatRect bounds;
+	// liste des éléments présents dans le noeud
+	private ArrayList<IGameBase> elements;
+	// references vers les 4 sous noeuds
+	private QuadTreeNode[] nodesFils;
+	// node level (permet de déterminer la hauteur maximal de l'arbre
+	private int levelNode;
+	
+	
+	public QuadTreeNode(int level,FloatRect bounds)
+	{
+		// initialisation du levelNode
+		levelNode = level;
+		
+		// creation du bounds
+		this.bounds = bounds;  
+		
+		// si le levelNode est supérieur au max de la hauteur des noeuds fils, on return
+		if(levelNode > QuadTree.LevelNodeMax)
+			return;
+		
+		// instantiation des nodes fils
+		nodesFils = new QuadTreeNode[4];
+		// initialisation
+		// création des bounds des fils nodes
+		FloatRect b1 =  new FloatRect(bounds.left,bounds.top,bounds.width/2,bounds.height/2);
+		FloatRect b2 =  new FloatRect(bounds.left + bounds.width /2,bounds.top,bounds.width/2,bounds.height/2);
+		FloatRect b3 =  new FloatRect(bounds.left,bounds.top + bounds.height / 2,bounds.width/2,bounds.height/2);
+		FloatRect b4 =  new FloatRect(bounds.left + bounds.width /2,bounds.top + bounds.height /2,bounds.width/2,bounds.height/2);
+		
+		// instance des 4 nodes fils
+		nodesFils[0] = new QuadTreeNode(levelNode + 1,b1);
+		nodesFils[1] = new QuadTreeNode(levelNode + 1,b2);
+		nodesFils[2] = new QuadTreeNode(levelNode + 1,b3);
+		nodesFils[3] = new QuadTreeNode(levelNode + 1,b4);
+		
+	
+	}
+	
+	public void InsertNode(IGameBase element)
+	{
+		
+		if(levelNode < QuadTree.LevelNodeMax)
+		{
+		
+			// l'element est-il dans le bounds ?
+			FloatRect result = bounds.intersection(element.GetGlobalBounds());
+			if(result != FloatRect.EMPTY)
+			{
+				// on descend dans les noeuds fils
+				nodesFils[0].InsertNode(element);
+				nodesFils[1].InsertNode(element);
+				nodesFils[2].InsertNode(element);
+				nodesFils[3].InsertNode(element);
+				
+			}
+		}
+		else
+		{
+			// si on a atteind le levelMax, on ajoute dans le noeud
+			elements.add(element);
+		}
+		
+	}
+
+	public FloatRect getBounds() {
+		return bounds;
+	}
+
+	public void setBounds(FloatRect bounds) {
+		this.bounds = bounds;
+	}
+	
+	
+}
