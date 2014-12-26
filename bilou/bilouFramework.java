b2e1b2111a2db7c56da6c9a3fc6a55c771c1b572
THOCED <thonon_cedric@hotmail.com>
2014-12-26 13:24:20 +0100

    Modification du framework pour les tests avec la nouvelle class quadtree

--- bilou/bilouFramework.java
+++ bilou/bilouFramework.java
@@ -41,7 +41,7 @@ public class bilouFramework
 		// camera
 		camera = new Camera(window);
 		// instance quadtree
-		quadtree = new QuadTreeNode(1,new FloatRect(0,0,10000,10000));
+		quadtree = new QuadTreeNode(1,new FloatRect(0,0,5120,5120));
 		// arrayunits
 		arrayElements = new ArrayList<IGameBase>();
 		//ListeElement
@@ -77,15 +77,13 @@ public class bilouFramework
 		listeElements.clear();
 		quadtree.GetElements(zone,listeElements);
 		
-		if(listeElements != null)
+		System.out.println("nb : " + String.valueOf(listeElements.size()) + " zone : x: " + zone.left + " y: " + zone.top + " width: " + zone.width + " height:" + zone.height);
+		
+		for(IGameBase e : listeElements)
 		{
-			System.out.println("nb : " + String.valueOf(listeElements.size()) + " zone : x: " + zone.left + " y: " + zone.top + " width: " + zone.width + " height:" + zone.height);
-			
-			for(IGameBase e : listeElements)
-			{
-				e.Draw(window);
-			}
+			e.Draw(window);
 		}
+		
 	}
 	
 	public void CatchEvent(Event event)
@@ -128,12 +126,18 @@ public class bilouFramework
 		ElementBase element2 = new ElementBase(new Vector2f(64,64),new Vector2f(192,128));
 		ElementBase element3 = new ElementBase(new Vector2f(64,64),new Vector2f(2048,128));
 		
-		this.quadtree.InsertElement(element);
-		this.quadtree.InsertElement(element2);
-		this.quadtree.InsertElement(element3);
+		for(int i=0;i<4;i++)
+		{
+			ElementBase element4 = new ElementBase(new Vector2f(64,64),new Vector2f(128 + i * 128,128 + i * 128));
+			this.quadtree.InsertElement(element4);
+		}
 		
-		arrayElements.add(element);
-		arrayElements.add(element2);
+		//this.quadtree.InsertElement(element);
+		//this.quadtree.InsertElement(element2);
+		//this.quadtree.InsertElement(element3);
+		
+		//arrayElements.add(element);
+		//arrayElements.add(element2);
 	}
 	
 	public void ReleaseContent()
