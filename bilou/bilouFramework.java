09e6520234c87ee089fecba108da9d31d525c9be
THOCED <thonon_cedric@hotmail.com>
2014-12-26 15:34:23 +0100

    modif de la framework pour utilisation de la drawdebugbounds du quadtree

--- bilou/bilouFramework.java
+++ bilou/bilouFramework.java
@@ -41,7 +41,7 @@ public class bilouFramework
 		// camera
 		camera = new Camera(window);
 		// instance quadtree
-		quadtree = new QuadTreeNode(1,new FloatRect(0,0,5120,5120));
+		quadtree = new QuadTreeNode(1,new FloatRect(0,0,5120*2,5120*2));
 		// arrayunits
 		arrayElements = new ArrayList<IGameBase>();
 		//ListeElement
@@ -83,6 +83,9 @@ public class bilouFramework
 		{
 			e.Draw(window);
 		}
+		
+		// drawdebug quadtree
+		quadtree.DrawDebugBounds(window);
 		
 	}
 	
@@ -126,9 +129,9 @@ public class bilouFramework
 		ElementBase element2 = new ElementBase(new Vector2f(64,64),new Vector2f(192,128));
 		ElementBase element3 = new ElementBase(new Vector2f(64,64),new Vector2f(2048,128));
 		
-		for(int i=0;i<4;i++)
+		for(int i=0;i<6;i++)
 		{
-			ElementBase element4 = new ElementBase(new Vector2f(64,64),new Vector2f(128 + i * 128,128 + i * 128));
+			ElementBase element4 = new ElementBase(new Vector2f(16,16),new Vector2f(128 + i * 64,128 + i * 64));
 			this.quadtree.InsertElement(element4);
 		}
 		
