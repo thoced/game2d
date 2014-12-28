1719de0f07e9d3a5068072906be587bc9df2b489
THOCED <thonon_cedric@hotmail.com>
2014-12-29 00:04:46 +0100

    modif de elementBase

--- bilou/ElementBase.java
+++ bilou/ElementBase.java
@@ -1,8 +1,16 @@
 package bilou;
 
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+
+import org.jsfml.graphics.Color;
 import org.jsfml.graphics.FloatRect;
+import org.jsfml.graphics.IntRect;
 import org.jsfml.graphics.RectangleShape;
+import org.jsfml.graphics.RenderStates;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
+import org.jsfml.graphics.Transform;
 import org.jsfml.system.Time;
 import org.jsfml.system.Vector2f;
 
@@ -14,8 +22,13 @@ public class ElementBase implements IGameBase
 	private float width;
 	private float height;
 	
+	private Transform trans;
+	
 	// Vue
 	private RectangleShape rectvue;
+	
+	// test texture
+	private Texture text;
 	
 	public ElementBase()
 	{
@@ -30,7 +43,7 @@ public class ElementBase implements IGameBase
 		height = dimension.y;
 		
 		rectvue = new RectangleShape(dimension);
-		rectvue.setOrigin(dimension.x /2,dimension.y /2);
+		rectvue.setOrigin(0,0);
 		rectvue.setPosition(position);
 		
 	}
@@ -43,8 +56,23 @@ public class ElementBase implements IGameBase
 		height = h;
 		
 		rectvue = new RectangleShape(new Vector2f(width,height));
-		rectvue.setOrigin(width /2,height /2);
+		rectvue.setOrigin(0,0);
 		rectvue.setPosition(new Vector2f(x,y));
+		rectvue.setOutlineColor(Color.RED);
+		rectvue.setOutlineThickness(2.0f);
+		
+		//debug
+		Texture text = new Texture();
+		try 
+		{
+			text.loadFromStream(ElementBase.class.getResourceAsStream("/Maps/texture.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		rectvue.setTexture(text,true);
+		//rectvue.setTextureRect(new IntRect(0,0,512,512));
+		
 		
 	}
 	
@@ -59,6 +87,8 @@ public class ElementBase implements IGameBase
 	public void Draw(RenderWindow window) 
 	{
 		// TODO Auto-generated method stub
+		//RenderStates state;
+		//state = new RenderStates(this.trans);
 		window.draw(rectvue);
 
 	}
@@ -84,4 +114,15 @@ public class ElementBase implements IGameBase
 		// TODO Auto-generated method stub
 		
 	}
+
+	public Transform getTrans() {
+		return trans;
+	}
+
+	public void setTrans(Transform trans) {
+		this.trans = trans;
+		
+	}
+	
+	
 }
