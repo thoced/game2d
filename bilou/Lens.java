19f8e3a215cc95d1a27d6c2d55d08ed9894abd8b
THOCED <thonon_cedric@hotmail.com>
2014-12-29 14:34:25 +0100

    initial commit (création de la class lens pour le test d'éclairage)

---
+++ bilou/Lens.java
@@ -0,0 +1,39 @@
+package bilou;
+
+import org.jsfml.graphics.FloatRect;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Time;
+
+public class Lens implements IGameBase {
+
+	@Override
+	public void Init(Framework parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Update(Time deltaTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Draw(RenderWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public FloatRect GetGlobalBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FloatRect GetLocalBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
