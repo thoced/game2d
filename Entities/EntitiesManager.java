49294cb02824b9bfaac1171b5116bc73278a6561
THOCED <thonon_cedric@hotmail.com>
2015-01-04 21:44:24 +0100

    modif

--- Entities/EntitiesManager.java
+++ Entities/EntitiesManager.java
@@ -1,36 +1,39 @@
 package Entities;
 
+import org.jsfml.graphics.RenderStates;
+import org.jsfml.graphics.RenderTexture;
 import org.jsfml.system.Time;
+import org.jsfml.system.Vector2f;
 import org.jsfml.window.event.Event;
 
 import bilou.ICoreBase;
 
 public class EntitiesManager implements ICoreBase
 {
-	// Player
+	
 	private PlayerView player;
 	
 	
 	public EntitiesManager()
 	{
 		player = new PlayerView();
+		player.SetPosition(new Vector2f(96,10));
 	}
 	@Override
-	public void Update(Time deltaTime) {
+	public void Update(Time deltaTime)
+	{
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void Draw() {
-		// TODO Auto-generated method stub
-		
+		player.Update(deltaTime);
 	}
 
+	
+
 	@Override
-	public void LoadContent() {
+	public void LoadContent() 
+	{
 		// TODO Auto-generated method stub
-		
+		//Load content du TextureManager
+		player.LoadContent();
 	}
 
 	@Override
@@ -56,6 +59,13 @@ public class EntitiesManager implements ICoreBase
 	{
 		// TODO Auto-generated method stub
 		this.player.SetEvent(e);
+	}
+	@Override
+	public void Draw(RenderTexture render, RenderStates state) 
+	{
+		
+		render.draw(player.getSpritePlayer());
+		
 	}
 	
 }
