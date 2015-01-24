97728c16af3e70d9906bfd32862e057006ee19fb
THOCED <thonon_cedric@hotmail.com>
2015-01-24 11:11:17 +0100

    ajout de la methode update

--- CoreDrawableCalqueManager/DrawableCalque.java
+++ CoreDrawableCalqueManager/DrawableCalque.java
@@ -4,16 +4,14 @@ import org.jsfml.graphics.FloatRect;
 import org.jsfml.graphics.RenderTexture;
 import org.jsfml.graphics.Sprite;
 import org.jsfml.graphics.Texture;
+import org.jsfml.system.Time;
 import org.jsfml.system.Vector2f;
 
 import bilou.Camera;
 
-public class DrawableCalque
+public class DrawableCalque extends DrawabkeCalqueBase
 {
-	// sprite du layer à afficher
-	private Sprite imageCalque;
-	// nom du calque
-	private String name;
+	
 	
 	public DrawableCalque(Texture text,String name,int posx,int posy)
 	{
@@ -31,6 +29,12 @@ public class DrawableCalque
 		FloatRect result = Camera.GetBoundsVisible().intersection(this.imageCalque.getGlobalBounds());
 		if(result!=null)
 			render.draw(imageCalque);
+		
+	}
+
+	@Override
+	public void Update(Time deltaTime) {
+		// TODO Auto-generated method stub
 		
 	}
 	
