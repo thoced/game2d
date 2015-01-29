d3beb265be5560e3de770b902362c695d12b24b0
THOCED <thonon_cedric@hotmail.com>
2015-01-29 23:16:18 +0100

    Modif

--- Entities/EntitiesManager.java
+++ Entities/EntitiesManager.java
@@ -6,24 +6,52 @@ import org.jsfml.system.Time;
 import org.jsfml.system.Vector2f;
 import org.jsfml.window.event.Event;
 
+import bilou.Camera;
 import bilou.ICoreBase;
 
 public class EntitiesManager implements ICoreBase
 {
-	
+	// small robot
 	private SmallRobotView playerSmallRobot;
+	// big robot
+	private BigRobotView playerBigRobot;
 	
+	// reférence vers le robot selectioné
+	private EntitieBase playerSelected;
 	
 	
 	public EntitiesManager()
 	{
 		
 	}
+	
+	public void SwitchPlayer()
+	{
+		// inverse la selection
+		if(playerSmallRobot.pControl.isSelected)
+		{
+			playerSmallRobot.pControl.setSelected(false);
+			playerBigRobot.pControl.setSelected(true);
+			
+		}
+		else
+		{
+			playerSmallRobot.pControl.setSelected(true);
+			playerBigRobot.pControl.setSelected(false);
+			
+		}
+		
+		
+		
+		
+	}
+	
 	@Override
 	public void Update(Time deltaTime)
 	{
 		// TODO Auto-generated method stub
 		playerSmallRobot.Update(deltaTime);
+		playerBigRobot.Update(deltaTime);
 	}
 
 	
@@ -36,6 +64,13 @@ public class EntitiesManager implements ICoreBase
 		playerSmallRobot = new SmallRobotView();
 		playerSmallRobot.LoadContent();
 		playerSmallRobot.SetPosition(new Vector2f(10,0));
+		
+		playerBigRobot = new BigRobotView();
+		playerBigRobot.LoadContent();
+		playerBigRobot.SetPosition(new Vector2f(30,0));
+		
+		// initialisation du robot selectionné
+		playerSelected = playerSmallRobot;
 	}
 
 	@Override
@@ -60,13 +95,16 @@ public class EntitiesManager implements ICoreBase
 	public void CatchEvent(Event e) 
 	{
 		// TODO Auto-generated method stub
-		this.playerSmallRobot.SetEvent(e);
+		this.playerSelected.SetEvent(e);
+		
 	}
 	@Override
 	public void Draw(RenderTexture render, RenderStates state) 
 	{
 		
 		render.draw(playerSmallRobot.getSpritePlayer());
+		
+		render.draw(playerBigRobot.getSpritePlayer());
 		
 	}
 	
