ac0ef88a9a2a96cd10f700567651db970d394a5b
THOCED <thonon_cedric@hotmail.com>
2015-01-29 23:16:26 +0100

    modif

--- Entities/RobotBase.java
+++ Entities/RobotBase.java
@@ -1,12 +1,34 @@
 package Entities;
 
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.FixtureDef;
 import org.jsfml.system.Time;
+import org.jsfml.system.Vector2f;
 import org.jsfml.window.event.Event;
+
 
 public class RobotBase extends EntitieBase 
 {
 	// est il sur le ground
-	private boolean isground = false;
+	protected boolean isground = false;
+	// est il selectionné
+	protected boolean isSelected = false;
+	// Vecteur de position
+	protected Vector2f positionPlayer;
+	// enum sens
+	public enum SENS {PAUSE,GAUCHE,DROITE}; 
+		// direction choisie (pour animation)
+	protected SENS typeSens = SENS.PAUSE;
+	// body physic
+	protected Body body;
+		// bodydef
+	protected BodyDef bodyDef;
+		
+	protected FixtureDef fixture;
+		
+	protected Fixture ff;
 	
 	@Override
 	public void Update(Time elapsedTime) {
@@ -45,6 +67,59 @@ public class RobotBase extends EntitieBase
 	public void setIsground(boolean isground) {
 		this.isground = isground;
 	}
+
+	/**
+	 * @return the isSelected
+	 */
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	/**
+	 * @param isSelected the isSelected to set
+	 */
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+
+	/**
+	 * @return the typeSens
+	 */
+	public SENS getTypeSens() {
+		return typeSens;
+	}
+	
+	/**
+	 * @return the body
+	 */
+	public Body getBody() {
+		return body;
+	}
+
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody(Body body) {
+		this.body = body;
+	}
+	
+	/**
+	 * @param positionPlayer the positionPlayer to set
+	 */
+	public void setPositionPlayer(Vector2f positionPlayer) 
+	{
+		this.positionPlayer = positionPlayer;
+		this.UpdateAttachMVC();
+	}
+	/**
+	 * @return the positionPlayer
+	 */
+	public Vector2f getPositionPlayer() {
+		return positionPlayer;
+	}
+
+
+	
 	
 	
 
